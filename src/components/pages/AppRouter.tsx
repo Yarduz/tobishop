6836054fc8a4312c74d9c57163e61a9ea75ac339
@bobfalafel <shayshun3@gmail.com>
2023-03-06 16:43:24 +0200

    idk

--- src/components/pages/AppRouter.tsx
+++ src/components/pages/AppRouter.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
