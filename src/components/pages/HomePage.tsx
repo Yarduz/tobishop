5b088f4fe01f7d53c759183e0338b9ace83fc00f
Yarduz <yaloden12@gmail.com>
2023-03-06 11:31:48 +0200

    ralbad

---
+++ src/components/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './HomePage.css';
+
+function HompePage() {
+    return (
+        <div className="App">
+            homepage
+        </div>
+    );
+}
+
+export default HompePage;
